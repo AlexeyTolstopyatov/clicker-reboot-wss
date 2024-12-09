704b98d6c283d3fd0d7f0f75c6f351fedb998298
Alexey Tolstopyatov <att33pod@gmail.com>
2024-12-09 23:04:35 +0700

    remove unused driver commit

--- internal/driver/postgres/postgresManipulativeImpl.go
+++
@@ -1,6 +0,0 @@
-package postgres
-
-//
-// Here contains data manipulation language
-// or many kinds of SQL Data manipulating instructions
-//
--- internal/driver/postgres/postgresQueryableImpl.go
+++
@@ -1,5 +0,0 @@
-package postgres
-
-//
-// Here contains data query language or many kinds of one SQL instruction
-//
